f5298462db5b5760c9a6615777830f6104c33111
chaeeah <dncodk@naver.com>
2025-07-19 16:34:04 +0900

    Feat: 회원가입 및 로그인 api 구현 #1

---
+++ src/main/java/com/devsong/server/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.devsong.server.user.controller;
+
+import com.devsong.server.user.dto.*;
+import com.devsong.server.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup") //회원가입
+    public SignupResponseDto signup(@RequestBody SignupRequestDto signupRequestDto) {
+        return userService.signup(signupRequestDto);
+    }
+
+    @PostMapping("/login") //로그인
+    public LoginResponseDto login(@RequestBody LoginRequestDto loginRequestDto) {
+        return userService.login(loginRequestDto);
+    }
+}
