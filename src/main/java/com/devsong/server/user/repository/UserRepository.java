18199cd65e1e53bb3ce413431f755793203060e9
chaeeah <dncodk@naver.com>
2025-07-19 16:45:30 +0900

    Feat: UserRepository 정의 #1

---
+++ src/main/java/com/devsong/server/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.devsong.server.user.repository;
+
+import com.devsong.server.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
