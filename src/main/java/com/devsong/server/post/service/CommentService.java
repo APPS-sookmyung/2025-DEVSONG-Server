0eb803a2ca237fba0435c98dc1e9d56c401605ab
chaeeah <dncodk@naver.com>
2025-08-26 22:43:15 +0900

    #12 Fix: 댓글 달기 api에 jwt 적용

--- src/main/java/com/devsong/server/post/service/CommentService.java
+++ src/main/java/com/devsong/server/post/service/CommentService.java
@@ -10,6 +10,8 @@ import com.devsong.server.user.entity.User;
 import com.devsong.server.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,8 +24,13 @@ public class CommentService {
 
     @Transactional
     public CommentResponseDto addComment(CommentRequestDto dto) {
-        User user = userRepository.findById(dto.getUserId())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        //jwt로 유저 정보 얻기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Long userId = (Long) auth.getPrincipal();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유저 정보를 찾을 수 없습니다."));
 
         Post post = postRepository.findById(dto.getPostId())
                 .orElseThrow(() -> new RuntimeException("Post not found"));
