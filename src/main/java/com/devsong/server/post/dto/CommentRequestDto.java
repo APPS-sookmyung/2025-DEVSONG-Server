b2f9ed5d429dcd6d8c3eb8bf521d2f99427a2462
Bangjh05 <everangel05@naver.com>
2025-08-30 15:01:51 +0900

    #32 fix: userId 삭제

--- src/main/java/com/devsong/server/post/dto/CommentRequestDto.java
+++ src/main/java/com/devsong/server/post/dto/CommentRequestDto.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CommentRequestDto {
     private Long postId;
-    private Long userId;
     private String content;
     private Long parentId;
 }
