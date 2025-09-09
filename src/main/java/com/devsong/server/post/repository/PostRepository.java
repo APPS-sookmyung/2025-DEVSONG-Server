d8599bb482e8ba68bc82a28d4c9f32746251828f
Bangjh05 <everangel05@naver.com>
2025-09-09 21:10:32 +0900

    #39 feat: 현재 인기글 조회 (베스트 글 불러오기) 기능 구현

--- src/main/java/com/devsong/server/post/repository/PostRepository.java
+++ src/main/java/com/devsong/server/post/repository/PostRepository.java
@@ -2,11 +2,17 @@ package com.devsong.server.post.repository;
 
 import com.devsong.server.post.entity.Category;
 import com.devsong.server.post.entity.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByIdDesc(); //전체조회
     List<Post> findAllByCategoryOrderByIdDesc(Category category); //카테고리별 조회
+    @Query("SELECT p FROM Post p LEFT JOIN PostLike pl ON p.id = pl.post.id " +
+            "GROUP BY p.id " +
+            "ORDER BY COUNT(pl.id) DESC")
+    List<Post> findByLikeCountDesc(Pageable pageable); //좋아요 상위 9개 게시글 조회
 }
